60674f3149d279190872ba47382a97d2ec0e12e2
NgocAnhVu18 <ngocanhvu18xd@gmail.com>
2018-05-26 11:05:59 +0700

    Initial Commit

---
+++ SearchDemoCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchDemoCode
+//
+//  Created by Ngọc Anh on 5/26/18.
+//  Copyright © 2018 Ngọc Anh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
